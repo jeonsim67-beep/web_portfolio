ea5d822754e777bf00a3b6f210705555c3142566
jeonsim67-beep <jeonsim67@gmail.com>
2026-06-25 23:44:47 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ document.addEventListener('mousemove', (e) => {
 });
 
 /* 메뉴에 마우스를 올리면 커서 확대 */
-const menuItems = document.querySelectorAll(".menu a , .contact_btn, .contact_button");
+const menuItems = document.querySelectorAll(
+    ".logo, .logo img, .menu a, .contact_btn, .contact_button"
+);
 menuItems.forEach(item => {
     item.addEventListener("mouseenter", () => {
         cursor.classList.add("active");
@@ -19,4 +21,57 @@ menuItems.forEach(item => {
     item.addEventListener("mouseleave", () => {
         cursor.classList.remove("active");
     });
+
+    const counters = document.querySelectorAll(".counter");
+
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+                const counter = entry.target;
+                const target = +counter.dataset.target;
+
+                let current = 0;
+
+                const increment = target / 80;
+
+                const update = () => {
+
+                    current += increment;
+
+                    if (current < target) {
+
+                        counter.innerText = Math.floor(current);
+
+                        requestAnimationFrame(update);
+
+                    } else {
+
+                        if (target >= 1000) {
+
+                            counter.innerText = target + "+";
+
+                        } else {
+
+                            counter.innerText = target;
+
+                        }
+
+                    }
+
+                }
+
+                update();
+
+                observer.unobserve(counter);
+
+            }
+
+        });
+
+    });
+
+    counters.forEach(counter => observer.observe(counter));
 });
